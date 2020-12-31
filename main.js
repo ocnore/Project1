4da4a503a7ecf2ec2290da783ce27a4def0d4c9a
Dmitry <jazbig@yandex.by>
2020-12-31 22:24:34 +0300

    movies

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-alert ('hello')
+const answer = prompt('How much do you watch films');
+
+const personalMovieDB = {
+    count: answer,
+    movies: {},
+    actors: {},
+    geners: [],
+    privat: false
+};
+
+const lastFilm = prompt('What was last film you see?');
+const lastFilmRate = prompt('What rate is it?');
+
+personalMovieDB.movies.lastFilm = lastFilmRate;
