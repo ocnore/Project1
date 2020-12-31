0106e6e9ad44787fab29fea3a5e30d07ae464508
Dmitry <jazbig@yandex.by>
2020-12-31 23:16:04 +0300

    task 2

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-const answer = prompt('How much do you watch films');
+'use strict';
+const answer = +prompt('How much do you watch films');
+
 
 const personalMovieDB = {
     count: answer,
@@ -8,7 +10,37 @@ const personalMovieDB = {
     privat: false
 };
 
-const lastFilm = prompt('What was last film you see?');
-const lastFilmRate = prompt('What rate is it?');
+if (personalMovieDB.count == 2) {
+    alert (`too little`);
+}
+else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+    alert (`you are classic`);
+}
+else if (personalMovieDB.count > 30) {
+    alert (`kinoman`);
+}
+else {
+    alert (`error`);
+}
 
-personalMovieDB.movies.lastFilm = lastFilmRate;
+if (personalMovieDB.count > 0 && Number.isInteger(personalMovieDB.count)) {
+    for (let index = 0; index < answer; index++) {
+        let lastFilm;
+        for (let index = 0; index < 1;) {
+            lastFilm = prompt('What was last film you see?');
+            if (lastFilm == null) {
+                continue;}
+            else if (lastFilm,length < 50 && lastFilm.length > 0){
+                break;
+            }
+            
+        }        
+        let lastFilmRate = prompt('What rate is it?');
+        
+        personalMovieDB.movies[lastFilm] = lastFilmRate;       
+    }
+}
+
+console.log(personalMovieDB.movies);
+
+
